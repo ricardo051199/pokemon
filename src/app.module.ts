190ea2a95ecf3cda505020249ea319c3e231560e
Ricardo <jesus.pelaez@ucb.edu.bo>
2022-11-30 13:58:39 -0400

    crear recursos (gimnasios, lideres, pokemons)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { LeadersModule } from './resourses/leaders/leaders.module';
+import { GymsModule } from './resourses/gyms/gyms.module';
+import { PokemonsModule } from './resourses/pokemons/pokemons.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, LeadersModule, GymsModule, PokemonsModule],
   controllers: [AppController],
   providers: [AppService],
 })
